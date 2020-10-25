fc26b53ae3596cf55936b2ed313c63c7fded7cf8
Пронин Егор <mrzillagold@gmail.com>
2020-10-25 13:57:10 +1000

    Скрипт для исправления сборки

---
+++ scripts/fixImports.mjs
@@ -0,0 +1,19 @@
+import { promises as fs } from "fs";
+
+fs.readdir("./lib")
+    .then((files) => {
+        files.forEach(async (file) => {
+            if (file.endsWith(".js")) {
+                const filePath = `./lib/${file}`;
+
+                fs.readFile(filePath)
+                    .then((file) => {
+                        file = file.toString();
+
+                        file = file.replace(/\.default;$/gm, ";");
+
+                        fs.writeFile(filePath, file);
+                    });
+            }
+        })
+    });
