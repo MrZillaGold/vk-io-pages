7190dd47b54ece4a98286c7e6953cc23c104a185
Пронин Егор <mrzillagold@gmail.com>
2021-01-08 20:39:10 +1000

    Изменение возвращаемого типа триггером

--- src/interfaces/trigger.ts
+++ src/interfaces/trigger.ts
@@ -2,7 +2,7 @@ import { IPagesBuilderOptions } from "./pagesBuilder";
 
 export interface ITrigger {
     name: "string";
-    callback: (context: IPagesBuilderOptions["context"]) => void;
+    callback: (context: IPagesBuilderOptions["context"]) => unknown;
 }
 
 export type TriggersMap = Map<ITrigger["name"], ITrigger["callback"]>;
