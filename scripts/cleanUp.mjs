4d86fa45d9681c81b123084de03834fa78725745
Пронин Егор <mrzillagold@gmail.com>
2021-05-06 01:42:23 +1000

    chore(scripts): cleanUp script

---
+++ scripts/cleanUp.mjs
@@ -0,0 +1,10 @@
+import { promises as fs } from "fs";
+
+import tsconfig from "../tsconfig.json";
+
+const OUT_DIR = tsconfig.compilerOptions.outDir;
+
+await fs.rm(`./${OUT_DIR}`, {
+    force: true,
+    recursive: true
+});
