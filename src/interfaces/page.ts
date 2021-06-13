625056c606b3255aa7f85819ff825bfd0f8bd740
Пронин Егор <mrzillagold@gmail.com>
2021-06-14 07:21:11 +1000

    fix(page): allow promise

--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -9,7 +9,7 @@ type ExtendedMessageContextOptions = IMessageContextSendOptions & {
 
 export type StringPage = string;
 export type ObjectPage = ExtendedMessageContextOptions;
-export type FunctionPage = (context: IPagesBuilderOptions["context"]) => StringPage | ObjectPage;
+export type FunctionPage = (context: IPagesBuilderOptions["context"]) => StringPage | ObjectPage | Promise<ObjectPage | StringPage>;
 
 export type Page = StringPage | ObjectPage | FunctionPage;
 export type PageSentMethod = "send_new" | "edit";
